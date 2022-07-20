7560cab25ea01e9432d952a471459ee99967d51f
Revanth V S <60284301+revanthvsr@users.noreply.github.com>
2022-07-20 12:55:45 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -7,7 +7,7 @@ function AgeCalculator()
         button.addEventListener("click",function(event){ 
             event.preventDefault();
             fname.checkValidity();
-            const fullname=fname["value"];
+            const fullname=fname["value"];fname["value"]==="" || birth["value"]==="" ? alert("Please enter the values") : 
             const userSelectedYear =new Date(birth["value"]).getFullYear();
             const userSelectedMonth=new Date(birth["value"]).getMonth();
             const userSelectedDay=new Date(birth["value"]).getDate();
@@ -15,7 +15,7 @@ function AgeCalculator()
             const month=new Date().getMonth()-userSelectedMonth;
             const day=new Date().getDate()-userSelectedDay;
             
-           return document.getElementById('demo').innerHTML=("The Age of "+fullname+" is "+age+" years "+month+ " months "+day+ " days");
+           return fname["value"]==="" || birth["value"]==="" ? alert("Please enter the values") : document.getElementById('demo').innerHTML=("The Age of "+fullname+" is "+age+" years "+month+ " months "+day+ " days");
             
         });
     
